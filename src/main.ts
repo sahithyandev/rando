8fadf7497599a4f44a376deb67433e483c616c67
Sahithyan <sahithyan2701@gmail.com>
2024-04-23 23:13:22 +0530

    fix: add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import * as pageView from "./handlers/page-view";
 
+const CORS_HEADERS = {
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+		"Access-Control-Allow-Headers": "Content-Type",
+	},
+};
+
 Bun.serve({
-	fetch(req) {
-		const url = new URL(req.url);
+	fetch(request) {
+		const url = new URL(request.url);
+
+		// Handle CORS preflight requests
+		if (request.method === "OPTIONS") {
+			return new Response("Departed", CORS_HEADERS);
+		}
 
 		switch (url.pathname) {
 			case "/page-view":
-				return pageView.GET(req);
+				return pageView.GET(request);
 			default:
 				return new Response("Not found", {
 					status: 404,
