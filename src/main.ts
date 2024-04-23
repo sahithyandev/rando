e472b7f534dc4ecf2f7d94ab8ce014df56b64ab8
Sahithyan <sahithyan2701@gmail.com>
2024-04-23 18:44:20 +0530

    chore: add hostname

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import * as pageView from "./handlers/page-view";
 
 Bun.serve({
+	hostname:
+		process.env.NODE_ENV === "production" ? "rando.sahithyan.dev" : "0.0.0.0",
 	fetch(req) {
 		const url = new URL(req.url);
 
 		switch (url.pathname) {
 			case "/page-view":
 				return pageView.GET(req);
-				break;
 			default:
 				return new Response("Not found", {
 					status: 404,
