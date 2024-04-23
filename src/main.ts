5e60b17c88c5227aaea91218c0d4331d96820618
Sahithyan <sahithyan2701@gmail.com>
2024-04-23 23:08:31 +0530

    chore: remove hostname

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as pageView from "./handlers/page-view";
 
 Bun.serve({
-	hostname:
-		process.env.NODE_ENV === "production" ? "rando.sahithyan.dev" : "0.0.0.0",
 	fetch(req) {
 		const url = new URL(req.url);
 
