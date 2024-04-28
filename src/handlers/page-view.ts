8c01957f409a51eb3ffad0c0ba305091b3bae715
Sahithyan <sahithyan2701@gmail.com>
2024-04-28 13:27:35 +0530

    feat: page-view now supports multiple domains

--- src/handlers/page-view.ts
+++ src/handlers/page-view.ts
@@ -11,6 +11,17 @@ const RESPONSE_MAX_AGE = 108000; // 30 hours in seconds
  */
 const propertyId = "356543625";
 
+const propertyIdMap = Object.freeze({
+	"sahithyan.dev": "356543625",
+	"kalvi.lk": "398313900",
+});
+
+function isPropertyAvailableForDomain(
+	domain: string
+): domain is keyof typeof propertyIdMap {
+	return domain in propertyIdMap;
+}
+
 process.env.GOOGLE_APPLICATION_CREDENTIALS = join(
 	process.cwd(),
 	"./blog-pageviews-1713811580440-51ea0fe058d1.json"
@@ -35,11 +46,34 @@ export async function GET(
 ) {
 	const url = new URL(request.url);
 	const pagePath = url.searchParams.get("path");
-	console.log("pagePath", pagePath);
+
+	let targetDomain = url.searchParams.get("domain");
+	if (!targetDomain) {
+		request.headers.forEach((value, key) => {
+			console.log("header", key, value);
+		});
+		const hostHeader = request.headers.get("Host");
+		if (hostHeader) {
+			targetDomain = hostHeader.split(":")[0];
+		}
+	}
+
+	console.log("getting page views for", targetDomain, "on", pagePath);
+	if (!targetDomain || !isPropertyAvailableForDomain(targetDomain)) {
+		return new Response(
+			"Unsupported domain",
+			merge<ResponseInit>(
+				{
+					status: 400,
+				},
+				responseInitOptions
+			)
+		);
+	}
 
 	const runReportOptions: protos.google.analytics.data.v1beta.IRunReportRequest =
 		{
-			property: `properties/${propertyId}`,
+			property: `properties/${propertyIdMap[targetDomain]}`,
 			dimensions: [{ name: "pagePath" }],
 			metrics: [{ name: "screenPageViews" }],
 			dateRanges: [{ startDate: "2022-03-05", endDate: "yesterday" }],
