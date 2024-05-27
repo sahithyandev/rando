14bfe32829ce0c33aabe61f18b9fdc32e28da532
Sahithyan <sahithyan2701@gmail.com>
2024-05-28 01:44:43 +0530

    add cors

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 	"net/http"
 	"os"
 	"rando-api/internal"
+
+	"github.com/rs/cors"
 )
 
 func main() {
-	http.HandleFunc("/", internal.GetIndex)
-	http.HandleFunc("/page-view", internal.GetPageView)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", internal.GetIndex)
+	mux.HandleFunc("/page-view", internal.GetPageView)
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
@@ -19,7 +23,8 @@ func main() {
 	fmt.Printf("Running on port %s\n", PORT)
 	PORT = fmt.Sprintf(":%s", PORT)
 
-	err := http.ListenAndServe(PORT, nil)
+	handler := cors.Default().Handler(mux)
+	err := http.ListenAndServe(PORT, handler)
 
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
